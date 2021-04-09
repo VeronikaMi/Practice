75b1bc927f109d1ba1b78150b9db4d9dda1351c2
Veronika Mitichashvili <nika.mitichashili@gmail.com>
2021-04-10 00:47:40 +0400

    First day of work

---
+++ bootsrap + sass (Bookstore)/main.js
@@ -0,0 +1,12 @@
+var myCarousel = document.querySelector('#carouselExampleControls');
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: false,
+  wrap: true
+});
+
+var myCarousel2 = document.querySelector('#carouselExampleControls2');
+var carousel2 = new bootstrap.Carousel(myCarousel2, {
+  interval: false,
+  wrap: true
+});
+
