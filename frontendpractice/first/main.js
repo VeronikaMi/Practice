7b8bbaae065dc3aa949118d0cd00ec711bc71f8e
Veronika Mitichashvili <nika.mitichashili@gmail.com>
2021-05-11 22:19:32 +0400

    functional done(add > to links to a-s in footer  ), responsiveness(done only for big screens)

--- frontendpractice/first/main.js
+++ frontendpractice/first/main.js
@@ -1,11 +1,27 @@
 const moreBtn = document.querySelector(".spec");
 const menuOnMore = document.querySelector(".menu");
-const root = document.querySelector(":root");
+
+// const root = document.querySelector(":root");
+
+const fixedMenu = document.querySelector(".fixedMenu");
+const height = fixedMenu.offsetTop;
+console.log(height);
+window.onscroll = () => fixMenu();
 
 moreBtn.addEventListener('click',()=>{
   menuOnMore.classList.toggle("show");
+  moreBtn.querySelector(".vBar").classList.toggle("hide");
 
-  console.log(menuOnMore.classList.contains("show"));
-  menuOnMore.classList.contains("show") ? root.style.setProperty("--plusOrMinus", `"-"`) 
-                                        : root.style.setProperty("--plusOrMinus", `"+"`);
+  // console.log(moreBtn.querySelector(".vBar").classList.toggle("hide"));
+
+  // menuOnMore.classList.contains("show") ? root.style.setProperty("--plusOrMinus", `"-"`) 
+  //                                       : root.style.setProperty("--plusOrMinus", `"+"`);
 })
+
+const fixMenu = () => {
+  if (window.pageYOffset > height) {
+    fixedMenu.classList.add("fixed");
+  } else {
+    fixedMenu.classList.remove("fixed");
+  }
+}
